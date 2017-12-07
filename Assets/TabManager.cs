8ca1ec25794b46a662669eb52940a5510e31e161
Technifutur <e-warning@hotmail.com>
2017-12-07 15:25:28 +0100

    submenu

---
+++ Assets/TabManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TabManager : MonoBehaviour
+{
+    #region Public Members
+
+    public Button m_selected;
+    public AnimationCurve m_animationCurve;
+
+    #endregion
+
+    #region Public void
+
+    public void ChangeSelect(Button _newSelection)
+    {
+        m_selected.transform.Find("Submenu").gameObject.SetActive(false);
+        m_selected = _newSelection;
+        m_selected.transform.Find("Submenu").gameObject.SetActive(true);
+    }
+
+    #endregion
+
+    #region System
+
+    private void Awake()
+    {
+        m_selected.transform.Find("Submenu").gameObject.SetActive(true);
+        m_selected.Select();
+    }
+
+    #endregion
+
+    #region Tools Debug and Utility
+
+    #endregion
+
+    #region Private and Protected Members
+
+    #endregion
+}
