5f7934c9d376196258fd50ea51b57ccee44f0caf
Technifutur <e-warning@hotmail.com>
2017-12-07 12:06:21 +0100

    First commit

---
+++ Assets/LevelLoader.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour
+{
+    #region Public Members
+
+    public Slider m_slider;
+    public Text m_text;
+
+    #endregion
+
+    #region Public void
+
+    public void LevelLoađing(int _levelToLoad)
+    {
+        StartCoroutine(LoadLevelAsync(_levelToLoad));
+    }
+
+    IEnumerator LoadLevelAsync(int _levelToLoad)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(_levelToLoad);
+
+        while ( !operation.isDone )
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+
+            Debug.Log("Chargement: " + operation.progress);
+
+            m_slider.value = progress;
+            m_text.text = (progress * 100) + " %";
+
+            yield return null;
+
+            // Same as:
+            // yield return new WaitForSeconds(0f);
+        }
+
+        
+    }
+
+    #endregion
+
+    #region System
+
+    void Start ()
+    {
+        
+    }
+
+    void Update ()
+    {
+        	
+    }
+
+    #endregion
+
+    #region Tools Debug and Utility
+
+    #endregion
+
+    #region Private and Protected Members
+
+    #endregion
+}
---
+++ Assets/UIManager.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    #region Public Members
+
+    public Animator m_animator;
+
+    public GameObject m_PlaySubMenu;
+
+    public GameObject m_panelAreYouSure;
+
+    public AudioSource m_hoverSound;
+
+    #endregion
+
+    #region Public void
+
+    public void ToggleCampaign(bool _status)
+    {
+        m_PlaySubMenu.SetActive(_status);
+
+        if (_status)
+        {
+            ToggleAreYouSure(false);
+            ToggleSettings(false);
+        }
+    }
+
+    public void ToggleAreYouSure(bool _status)
+    {
+        m_panelAreYouSure.SetActive(_status);
+
+        if (_status)
+        {
+            ToggleCampaign(false);
+            ToggleSettings(false);
+
+        }
+    }
+
+    public void ToggleSettings(bool _status)
+    {
+        m_animator.SetBool("Settings", _status);
+
+        if (_status)
+        {
+            ToggleCampaign(false);
+            ToggleAreYouSure(false);
+
+        }
+    }
+
+    public void AreYouSureAnswer(bool _quit)
+    {
+        if (_quit)
+        {
+            Application.Quit();
+            Debug.Log("QUITTING");
+            Debug.Break();
+        }
+        else
+            ToggleAreYouSure(false);
+    }
+
+    public void PlayOver()
+    {
+        m_hoverSound.Stop();
+        m_hoverSound.Play();
+    }
+
+    #endregion
+
+    #region System
+
+    private void Awake ()
+    {
+        if (m_animator == null)
+            m_animator = GetComponent<Animator>();
+    }
+
+    #endregion
+
+    #region Tools Debug and Utility
+
+    #endregion
+
+    #region Private and Protected Members
+
+    #endregion
+}
